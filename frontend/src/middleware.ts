ac44469698d8ab4ecc0e48a3cad56246880a3f92
Pratheek Shetty <pratheekshetty934@gmail.com>
2024-09-28 15:07:05 +0530

    auth fix

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -2,14 +2,14 @@
 import { NextResponse } from 'next/server';
 import { NextRequest } from 'next/server';
 export async function middleware(req: NextRequest) {
-  const res = await fetch('http://localhost:8079/check-auth', {
+  const res = await fetch('https://devhost-qr-reg.onrender.com/check-auth', {
     headers: {
       cookie: req.headers.get('cookie') || '',
     },
   });
   const userData= await res.json();
   if (res.status === 401) {
-    return NextResponse.redirect(new URL('http://localhost:8079/auth/google/', req.url));
+    return NextResponse.redirect(new URL('https://devhost-qr-reg.onrender.com/auth/google/', req.url));
   }
 else{
   if(userData.user.college&&req.nextUrl.pathname === '/register') return NextResponse.redirect(new URL('/events', req.url));
