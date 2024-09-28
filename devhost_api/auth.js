4930a41fb30b29a3e70a07e6513c23b27a28a3c3
Pratheek Shetty <pratheekshetty934@gmail.com>
2024-09-28 12:25:20 +0530

    redirect url fix 2

--- devhost_api/auth.js
+++ devhost_api/auth.js
@@ -6,7 +6,7 @@ require('dotenv').config()
 passport.use(new GoogleStrategy({
     clientID:     process.env.gclient_ID,
     clientSecret: process.env.gclient_SECRET,
-    callbackURL: `https://devhost-qr-reg.onrender.com/google-redirect`,
+    callbackURL: `https://devhost-qr-reg.onrender.com/google-callback`,
     passReqToCallback   : true
   },
   async function(request, accessToken, refreshToken, profile, done) {
