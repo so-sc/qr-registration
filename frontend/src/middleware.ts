5a92b2552c9b5bd49607c2f5f18f1355f0ee3654
Pratheek Shetty <pratheekshetty934@gmail.com>
2024-09-29 16:27:57 +0530

    middleware patch

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -34,5 +34,5 @@ export async function middleware(req: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/events','/edit', '/register'],
+  matcher: ['/events','/edit', '/register'],runtime: 'nodejs',
 };
