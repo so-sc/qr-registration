9f213f9683c5a01cd8d8e99eb4d7c94c9c43984a
Pratheek G Shetty <83195025+techshetty@users.noreply.github.com>
2024-09-30 22:15:54 +0530

    Middleware update and other minor changes (#13)
    * minor changes
    
    * redirect url fix
    
    * redirect url fix 2
    
    * netlify fix
    
    * netlify fix
    
    * netlify fix
    
    * netlify fix
    
    * netlify fix
    
    * auth fix
    
    * auth fix
    
    * netlify patch
    
    * api domain upd
    
    * test patch
    
    * devhost api update
    
    * nextjs middleware update
    
    * nextjs middleware update
    
    * razorpay patch
    
    * middleware update
    
    * middleware update
    
    * middleware update
    
    * middleware patch
    
    * middleware patch
    
    * middleware patch
    
    * middleware patch
    
    * middleware patch
    
    * middleware patch
    
    * middleware patch
    
    * middleware patch
    
    * middleware patch
    
    * middleware patch
    
    * small patch
    
    * middleware upd
    
    * auth endpoint patch
    
    * auth api patch
    
    * middleware upd
    
    * minor patch
    
    * minor patch
    
    * minor patch
    
    * minor patch
    
    * minor patch

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -1,21 +1,35 @@
-// src/middleware.ts
 import { NextResponse } from 'next/server';
 import { NextRequest } from 'next/server';
+import { cookies } from 'next/headers'
+
 export async function middleware(req: NextRequest) {
-  const res = await fetch('http://localhost:8079/check-auth', {
+  console.log(req.cookies)
+  const res = await fetch(`${process.env.NEXT_PUBLIC_APIHOST}/check-auth`, {
+    method: 'GET',
     headers: {
-      cookie: req.headers.get('cookie') || '',
+        'Content-Type': 'application/json',
+        'cookie': req.headers.get('cookie') || '',
     },
-  });
-  const userData= await res.json();
+    credentials: 'include',
+});
+
+  const userData = await res.json();
+  console.log('Auth Response:', userData);
+
+
   if (res.status === 401) {
-    return NextResponse.redirect(new URL('http://localhost:8079/auth/google/', req.url));
+    return NextResponse.redirect(new URL(`${process.env.NEXT_PUBLIC_APIHOST}/auth/google/`, req.url));
+    console.log("Auth Failed..redir to login")
+  } else {
+    if (userData.user?.college && req.nextUrl.pathname === '/register') {
+      return NextResponse.redirect(new URL('/events', req.url));
+    }
   }
-else{
-  if(userData.user.college&&req.nextUrl.pathname === '/register') return NextResponse.redirect(new URL('/events', req.url));
-}
+
   return NextResponse.next();
 }
+
 export const config = {
-  matcher: ['/events', '/register','/profile','/edit'],
+  matcher: [],
+  //'/events','/edit', '/register'
 };
